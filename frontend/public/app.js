1228861cab67b9ab1e321f6c11b74e4dff4752ea
Qwizi <500adrian2@wp.pl>
2019-06-26 12:46:41 +0200

    Dodanie przykladowego logowania, wylogowanywania. Autoryzacja przez token

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -1,15 +1,50 @@
-async function getTodos() {
-    let res = await axios.get('http://localhost:8000/api/todos/');
-    let todosDiv = document.getElementById('todos');
-    let todos = '<ul>';
-    for (var i = 0; i < res.data.length; i++) {
-        todos += "<li>" + res.data[i]['title'] + "</li>";
-    };
-    todos += '</ul>';
-    todosDiv.innerHTML = todos
-    console.log(res.data);
+
+let logged = localStorage.getItem('token') ? true : false;
+console.log(logged);
+console.log(localStorage.getItem('token'));
+
+let token = localStorage.getItem('token');
+let userData = [];
+
+async function getUserData() {
+    try {
+        res = await axios.get('http://localhost:8000/api/users/me/', {
+            headers: {'Authorization': `Token ${token}`}
+        })
+        userData = res.data;
+        console.log("Dane dodane");
+        console.log(userData);
+    } catch (error) {
+        console.log(error)
+    }
 }
-getTodos();
+
+if (logged) {
+    getUserData()
+}
+
+document.getElementById('login').addEventListener('click', async () => {
+    try {
+        res = await axios.post('http://localhost:8000/api-token-auth/', {
+            username: "admin",
+            password: "admin123"
+        });
+        localStorage.setItem('token', res.data.token);
+        logged = true;
+        console.log(res.data);
+        console.log('zalogowano');
+        location.reload();
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+document.getElementById('logout').addEventListener('click', () => {
+    localStorage.removeItem('token')
+    logged = false;
+    console.log('Wylogowano!')
+    location.reload();
+});
 
 async function getTodosWithForEach() {
     let res = await axios.get('http://localhost:8000/api/todos/');
