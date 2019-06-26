19d86f704172cb152e61337f096d8f0e72f223ed
Qwizi <500adrian2@wp.pl>
2019-06-26 10:43:37 +0200

    Testowanie petli forEach

--- frontend/public/app.js
+++ frontend/public/app.js
@@ -3,10 +3,19 @@ async function getTodos() {
     let todosDiv = document.getElementById('todos');
     let todos = '<ul>';
     for (var i = 0; i < res.data.length; i++) {
-        todos += "<li>" + res.data[i]['title'] + "</li>"
+        todos += "<li>" + res.data[i]['title'] + "</li>";
     };
     todos += '</ul>';
     todosDiv.innerHTML = todos
     console.log(res.data);
 }
 getTodos();
+
+async function getTodosWithForEach() {
+    let res = await axios.get('http://localhost:8000/api/todos/');
+    let todosUL = document.getElementById('todos');
+    res.data.forEach((item, index) => {
+        todosUL.innerHTML += "<li id=" + index + ">" + item['title'] + "</li>";
+    })
+}
+getTodosWithForEach();
